e2be0ffcc3a448d8f3876dc20e71fcb6798e02ab
inokinn <inokin1987@gmail.com>
2019-11-03 17:09:40 +0900

    計算するアプリを作成

---
+++ app/src/main/java/net/inokinn/fizzbuzztest/Calculator.kt
@@ -0,0 +1,33 @@
+package net.inokinn.fizzbuzztest
+
+import java.math.BigDecimal;
+
+class Calculator {
+
+    /**
+     * 指定した四則演算処理を行う
+     * @param num1Str 第1数
+     * @param num2Str 第2数
+     * @param symbolStr 四則演算記号
+     * @return 演算結果
+     */
+    fun execCalculation(num1Str: String, num2Str: String, symbolStr: String): Float {
+        // 入力値バリデーション
+        val num1 = num1Str.toFloatOrNull()
+        if (num1 == null) {
+            return 0.0f
+        }
+        val num2 = num2Str.toFloatOrNull()
+        if (num2 == null) {
+            return 0.0f
+        }
+
+        return when (symbolStr) {
+            "+" -> (BigDecimal((num1 + num2).toString()).setScale(2, BigDecimal.ROUND_HALF_UP)).toFloat()
+            "-" -> (BigDecimal((num1 - num2).toString()).setScale(2, BigDecimal.ROUND_HALF_UP)).toFloat()
+            "*" -> (BigDecimal((num1 * num2).toString()).setScale(2, BigDecimal.ROUND_HALF_UP)).toFloat()
+            "/" -> (BigDecimal((num1 / num2).toString()).setScale(2, BigDecimal.ROUND_HALF_UP)).toFloat()
+            else -> 0.0f
+        }
+    }
+}
