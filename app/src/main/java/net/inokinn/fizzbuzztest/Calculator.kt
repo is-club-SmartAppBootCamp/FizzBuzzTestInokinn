1db653401fdf40a48694ab992c8bb1f3a57219b6
inokinn <inokin1987@gmail.com>
2019-11-05 01:25:45 +0900

    KDocちゃんと書いた

--- app/src/main/java/net/inokinn/fizzbuzztest/Calculator.kt
+++ app/src/main/java/net/inokinn/fizzbuzztest/Calculator.kt
@@ -2,10 +2,14 @@ package net.inokinn.fizzbuzztest
 
 import java.math.BigDecimal;
 
+/**
+ * 計算を行うクラス.
+ */
 class Calculator {
 
     /**
      * 指定した四則演算処理を行う
+     *
      * @param num1Str 第1数
      * @param num2Str 第2数
      * @param symbolStr 四則演算記号
@@ -16,6 +20,7 @@ class Calculator {
         val num1 = num1Str.toFloatOrNull() ?: return 0.0f
         val num2 = num2Str.toFloatOrNull() ?: return 0.0f
 
+        // 四則演算
         return when (symbolStr) {
             "+" -> (BigDecimal((num1 + num2).toString()).setScale(2, BigDecimal.ROUND_HALF_UP)).toFloat()
             "-" -> (BigDecimal((num1 - num2).toString()).setScale(2, BigDecimal.ROUND_HALF_UP)).toFloat()
